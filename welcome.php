4077acabaeeb3e33dca0fbb2d4b35830ca5ea196
halimatanaj <161949794+halimatanaj@users.noreply.github.com>
2024-03-02 19:51:44 +0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        Welcome <?php echo $_POST["name"]; ?>.<br/>
+        you roll is <?php echo $_POST["roll"]; ?>.<br/>
+        you are the student of <?php echo $_POST["semester"]; ?> semester<br/>
+        your shift is <?php echo $_POST["shift"]; ?>.<br/>
+        your department is <?php echo $_POST["department"]; ?>.<br/>
+
+    </body>
+    </html>
